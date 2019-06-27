1ef294473f176393f0e527f2a628c0848da0e87f
d.zhelyazkov <d.zhelyazkov@ocado.com>
2019-06-27 10:58:09 +0300

    Added a new util for operations with Boolean variables.

---
+++ src/main/java/com/xrc/lang/Booleans.java
@@ -0,0 +1,11 @@
+package com.xrc.lang;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class Booleans {
+
+    public static Collection<Boolean> values() {
+        return Arrays.asList(Boolean.TRUE, Boolean.FALSE);
+    }
+}

0e0a57783d7c9abdb42c2a75560f134681c574c2
dzhelyazkov <d.zhelyazkov@ocado.com>
2019-08-09 17:50:03 +0300

    Implemented an awt.Dimension compare util.

---
+++ src/main/java/com/xrc/awt/DimensionByAreaComparator.java
@@ -0,0 +1,13 @@
+package com.xrc.awt;
+
+import java.awt.Dimension;
+import java.util.Comparator;
+
+public class DimensionByAreaComparator implements Comparator<Dimension> {
+    @Override
+    public int compare(Dimension d1, Dimension d2) {
+        int d1Area = d1.width * d1.height;
+        int d2Area = d2.width * d2.height;
+        return Integer.compare(d1Area, d2Area);
+    }
+}
---
+++ src/main/java/com/xrc/awt/Dimensions.java
@@ -0,0 +1,12 @@
+package com.xrc.awt;
+
+import java.awt.Dimension;
+import java.util.Comparator;
+
+public class Dimensions {
+    public static int compare(Dimension d1, Dimension d2) {
+
+        Comparator<Dimension> comparator = new DimensionByAreaComparator();
+        return comparator.compare(d1, d2);
+    }
+}

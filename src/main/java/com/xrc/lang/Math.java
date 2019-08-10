93ad0c480ce38bd6582ffb296ce8cd00e5dd77fb
dzhelyazkov <d.zhelyazkov@ocado.com>
2019-08-10 15:46:47 +0300

    Math#dir(double) switched  to Math#dir(Number)

--- src/main/java/com/xrc/lang/Math.java
+++ src/main/java/com/xrc/lang/Math.java
@@ -1,8 +1,9 @@
 package com.xrc.lang;
 
 public class Math {
-    public static int dir(double number) {
-        return (number == 0) ? 0 : (int) (number / java.lang.Math.abs(number));
+    public static int dir(Number number) {
+        double numberDouble = number.doubleValue();
+        return (numberDouble == 0) ? 0 : (int) (numberDouble / java.lang.Math.abs(numberDouble));
     }
 
     public static long factorial(int number) {
